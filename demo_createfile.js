e99e43923147576e810aadf75d29f4dd3a594000
Francisco Andre <55844517+Isco170@users.noreply.github.com>
2022-04-19 19:40:39 +0200

    Create files - open() method

--- demo_createfile.js
+++ demo_createfile.js
@@ -1,7 +1,14 @@
 
 var fs = require('fs')
 
-fs.appendFile('mynewfile.txt', 'Hello content!', function (err) {
-    if (err) throw err;
+// appendFile()
+// fs.appendFile('mynewfile.txt', 'Hello content!', function (err) {
+//     if (err) throw err;
+//     console.log('Saved!');
+// });
+
+// open()
+fs.open('mynewfile2.txt', 'w', function(err, file){
+    if(err) throw err;
     console.log('Saved!');
 });
