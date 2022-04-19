9c89c780bd4641ec9934c73dbf95a14b1e75f724
Francisco Andre <55844517+Isco170@users.noreply.github.com>
2022-04-19 20:00:53 +0200

    Read file, line by line

--- demo_readfile.js
+++ demo_readfile.js
@@ -1,10 +1,14 @@
-var http = require('http');
+// var http = require('http');
 var fs = require('fs')
 
-http.createServer(function(req, res){
-    fs.readFile('demofile.html', function(err, data){
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        return res.end();
-    });
-}).listen(8080);
+// http.createServer(function(req, res){
+//     fs.readFile('demofile.html', function(err, data){
+//         res.writeHead(200, {'Content-Type': 'text/html'});
+//         res.write(data);
+//         return res.end();
+//     });
+// }).listen(8080);
+
+fs.readFile('mynewfile3.txt', function(err, data){
+    console.log(data)
+})
